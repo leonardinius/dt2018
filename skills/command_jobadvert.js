7c8ece5994d5a48aa30e5cbaacf5729bcb342f3b
l.maslovs <leonids@printify.com>
2018-09-18 15:44:15 +0300

    cosmetics

--- skills/command_jobadvert.js
+++ skills/command_jobadvert.js
@@ -1,37 +1,36 @@
 module.exports = function(controller) {
-  const admins = require(__dirname + '/.conf/admins.json');
-  const sponsors = require(__dirname + '/.conf/sponsors.json');
+  const admins = require(__dirname + "/.conf/admins.json");
+  const sponsors = require(__dirname + "/.conf/sponsors.json");
   controller.on("slash_command", function(bot, message) {
-    var t = ''; //"\nMessage:```" + JSON.stringify(message) + "```";
+    var t = ""; //"\nMessage:```" + JSON.stringify(message) + "```";
 
-    if(admins.includes(message.username) <0){
-      bot.replyPrivate(message, "Only Staff Personell authorized to publish adverts." + t);
+    if (admins.includes(message.username) < 0) {
+      bot.replyPrivate( message, "Only Staff Personell authorized to publish adverts." + t );
       return;
     }
 
     if (message.command.startsWith("/jobadvert")) {
-      const parts = message.text.split('\n').join('\n ').split(' ');
+      const parts = message.text .split("\n") .join("\n ") .split(" ");
       let sponsorName = parts[0].trim();
-      while(sponsorName.startsWith('@')){
+      while (sponsorName.startsWith("@")) {
         sponsorName = sponsorName.substring(1);
       }
 
       const sponsor = sponsors[sponsorName.toLowerCase()];
-      if(!!!sponsor){
-        bot.replyPrivate(message, "Unsupported sponsor. Known sponsors are `" + Object.keys(sponsors).join(",") + "`" + t);
+      if (!!!sponsor) {
+        bot.replyPrivate( message, "Unsupported sponsor. Known sponsors are `" + Object.keys(sponsors).join(", ") + "`" + t );
         return;
       }
 
-      const text = parts.slice(1).join(' ').trim();
-
+      const text = parts.slice(1) .join(" ") .trim();
       bot.reply(message, {
         as_user: false,
-        username: 'Job@' + sponsor.name,
+        username: "Job@" + sponsor.name,
         icon_url: sponsor.logo,
         text: text + t,
         attachments: [
           {
-            text: "Do you like it?",
+            text: "",
             attachment_type: "default",
             callback_id: message.command,
             actions: [
@@ -40,7 +39,7 @@ module.exports = function(controller) {
                 text: "Like",
                 value: "like",
                 type: "button",
-                style: "primary",
+                style: "primary"
               },
               {
                 name: "ask-more",
@@ -53,14 +52,14 @@ module.exports = function(controller) {
           }
         ]
       });
-      bot.replyPrivate(message, 'Published');
+      bot.replyPrivate(message, "Published");
     } else {
       bot.replyPrivate(message, "Could not post job advert" + t);
     }
   });
 
   controller.on("interactive_message_callback", function(bot, message) {
-    var t = ''; //"\nMessage:```" + JSON.stringify(message).replace('```', 'code') + "```";
+    var t = ""; //"\nMessage:```" + JSON.stringify(message).replace('```', 'code') + "```";
     if (message.callback_id.startsWith("/jobadvert")) {
       bot.api.reactions.add(
         {
