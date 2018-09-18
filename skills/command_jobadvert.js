57b9a78635ea5df2e61e30d8ef482bc464845967
l.maslovs <leonids@printify.com>
2018-09-18 04:39:25 +0300

    wip

--- skills/command_jobadvert.js
+++ skills/command_jobadvert.js
@@ -4,8 +4,25 @@ module.exports = function(controller) {
       if(message.command == "/jobadvert"){
         // reply to slash command
         bot.replyPublic(
-          message,
-          "Everyone can see this part of the slash command" + t
+          message,{
+            text: message.text + t + "\n@" + message.user + " ask more",
+            attachments: [
+              {
+                text: "Do you like it?",
+                callback_id: "123",
+                attachment_type: "default",
+                actions: [
+                  {
+                    name: "like",
+                    text: "Like",
+                    value: "like",
+                    type: "button",
+                    style: "primary"
+                  }
+                ]
+              }
+            ]
+          }
         );
       } else {
         bot.replyPrivate(
