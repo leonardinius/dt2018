eec03a9cf392bfab429dd7633d39d97909361d0a
Eduards Sizovs <eduards.sizovs@gmail.com>
2018-10-09 15:15:07 +0300

    Update command_jobadvert.js

--- skills/command_jobadvert.js
+++ skills/command_jobadvert.js
@@ -5,19 +5,19 @@ module.exports = function(controller) {
   
   controller.hears('(.*)', "ambient", function(bot, message) {
     
-    let sponsor = message.user
+//     let sponsor = message.user
     
-    if (sponsors.indexOf(sponsor) < 0) {
-      bot.replyPrivate(message, "Sorry, only sponsors can publish job ads. Your message will be deleted.");
+//     if (sponsors.indexOf(sponsor) < 0) {
+//       bot.replyPrivate(message, "Sorry, only sponsors can publish job ads. Your message will be deleted.");
       
       
-      bot.api.chat.delete({token: message.token, ts: message.ts, channel: message.channel}, function(err, res) {
-        if (err) {
-          bot.botkit.log('chat.delete error: ', err);
-        }
-      });
+//       bot.api.chat.delete({token: message.token, ts: message.ts, channel: message.channel}, function(err, res) {
+//         if (err) {
+//           bot.botkit.log('chat.delete error: ', err);
+//         }
+//       });
       
-      return;
-    }   
-  });
+//       return;
+//     }   
+//   });
 }
